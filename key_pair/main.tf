384a2c8f5d55e165f3b175a71a5b9932b5054c68
shivareddy889 <shivakumarreddy814@gmail.com>
2025-07-30 20:23:39 +0530

    changes done

--- key_pair/main.tf
+++ key_pair/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "key_pair" {
     ami =  var.ami_id
     instance_type = var.instance_type
-    key_name = aws_key_pair.shiva_key
+    key_name = aws_key_pair.shiva_key.key_name
     count = var.instance_count
     tags = {
         Name = "key_pair_instance${count.index}"
